383f22d073903a5ca5c8f3bac0b0f8c9251d0067
Pablo Tabares Hernandez <pablother3@gmail.com>
2019-09-26 16:02:29 -0500

    screens and navigation file added

---
+++ src/screens/CocktailDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const CocktailDetail = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default CocktailDetail

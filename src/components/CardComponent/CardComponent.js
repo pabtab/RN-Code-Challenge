e77a32ba12b6469c083b6e19d40b34e2f11ec82f
Pablo Tabares Hernandez <pablother3@gmail.com>
2019-09-27 09:56:47 -0500

    card component created

---
+++ src/components/CardComponent/CardComponent.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { View, TouchableOpacity, Image } from 'react-native'
+import { Card, CardItem, Body, Text, Right, Grid, Col } from 'native-base';
+
+import Styles from './CardComponent.styles'
+
+const CardComponent = ({ onPress, title, image, children }) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Card style={Styles.card}>
+        <CardItem style={{borderStyle: 'dotted'}}>
+          
+            <Grid>
+              <Col size={70} style={Styles.container}>
+                <View style={Styles.titleContainer}>
+                  <Text style={Styles.title}>{title}</Text>
+                </View>
+                <View style={Styles.bodyContainer}>
+                  <Text style={Styles.body}>{children}</Text>
+                </View>
+              </Col>
+              <Col size={30} style={Styles.container}>
+                <View style={Styles.imageContainer}>
+                  <Image 
+                    source={image}
+                  />
+                </View>
+              </Col>
+            </Grid>
+            
+          
+        </CardItem>
+      </Card>
+    </TouchableOpacity>
+  )
+}
+
+export default CardComponent
---
+++ src/components/CardComponent/CardComponent.styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native'
+
+const card = StyleSheet.create({
+  card: {
+    height: 200,
+    borderRadius: 10,
+    
+  },
+  container: {
+    
+  },
+  titleContainer: {
+    height: 50
+  },
+  title: {
+    fontSize: 30,
+    color: 'red'
+  },
+  imageContainer: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'gray',
+  }
+})
+
+export default card
